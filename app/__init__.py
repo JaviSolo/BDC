bfcdfdb783c9aba79a2ae74608d9f7f0b9a76897
JaviSolo <javierleal90@gmail.com>
2025-04-21 22:40:50 +0200

    Setup Flask base structure and status endpoint

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from app.api import api_bp
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Register Blueprints
+    app.register_blueprint(api_bp, url_prefix="/api")
+
+    return app
